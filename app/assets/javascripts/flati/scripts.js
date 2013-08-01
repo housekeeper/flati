563e7ebcb83abcbc154a3a76050ce8660fdbd87d
daveed <david.alphen@gmail.com>
2013-08-02 07:48:57 +0800

    removing prettyPhoto js exception when no prettyPhoto element in the page

--- app/assets/javascripts/flati/scripts.js
+++ app/assets/javascripts/flati/scripts.js
@@ -114,14 +114,16 @@ $("[data-rel=tooltip]").tooltip();
 /***************************************************
 		PRETTYPHOTO
 ***************************************************/
-$('a[data-rel]').each(function() {
-$(this).attr('rel', $(this).attr('data-rel')).removeAttr('data-rel');
-});
-$("a[rel^='prettyPhoto']").prettyPhoto();
-	jQuery("a[rel^='prettyPhoto'], a[rel^='lightbox']").prettyPhoto({
-overlay_gallery: false, social_tools: false,  deeplinking: false
-});
-	
+	$('a[data-rel]').each(function() {
+  	$(this).attr('rel', $(this).attr('data-rel')).removeAttr('data-rel');
+	});
+	if ($("a[rel^='prettyPhoto']").size() > 0 || $("a[rel^='lightbox']").size() > 0) {
+		$("a[rel^='prettyPhoto']").prettyPhoto();
+		$("a[rel^='prettyPhoto'], a[rel^='lightbox']").prettyPhoto({
+			overlay_gallery: false, social_tools: false,  deeplinking: false
+		});
+	}
+
 });	
 /***************************************************
 	ANIMATIONS
